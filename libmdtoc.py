b300dadf9fd1fccc1d3174360cebb1e4d6c1dc31
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-04-27 18:06:44 +0100

    Make some documentation changes in libmdtoc.py.

--- libmdtoc.py
+++ libmdtoc.py
@@ -17,9 +17,11 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # import the sys module for standard IO and the re module for parsing regular expressions.
-import sys, re
+import sys
+import re
 # We'll also need the line separator which varies depending on your operating system.
 from os import linesep
+
 # Import some bits and pieces from the typing module to be used with type hints
 from typing import List, IO, Union
 
@@ -29,122 +31,126 @@ Strings = List[str]
 File = Union[str, IO['TextIO']]
 
 # Create the MDTOCItem class, which represents an item on the table of contents.
-class MDTOCItem(object):
-  # This is the constructor function, which sets up the object.
-  def __init__(self, level: int, title: Strings, min_indent: int=1):
-# Set some important properties.
-    self.level: int = level
-    self.title: Strings = title
-    self.min_indent: int = max(0, min_indent)
-  
-# This function joins a given list of strings with a given separator.
-  def join(self, words: Strings, separator: str=" ") -> str:
-    # Initialise the text variable to an empty string.
-    text: str = ""
-    # Loop through each of the words.
-    for word in words:
-      # Append the word, as well as a proceeding separator, to the text variable.
-      text += separator + word
-    # Remove the first proceeding separator from text and then return it.
-    separator_end: int = len(separator)
-    text = text[separator_end:]
-    return text
-  
-# This function returns a non-linked list item representing this list item. 
-  def get_list_item_nonlinked(self) -> str:
-# First, get a textual representation of the title.
-    title_text: str = self.join(self.title, " ")
-# Next, create white space based on the level of the item and the minimum indent level.
-    whitespace: str = "\t" * (self.level - self.min_indent)
-# Finally, return the full list item.
-    return whitespace + "* " + title_text
-  
-  # This function returns a linked list item representing this list item. 
-  def get_list_item_linked(self) -> str:
-# First, get a textual representation of the title.
-    title_text: str = self.join(self.title, " ")
-# Also, get a textual representattion of a link reference to this heading.
-    title_reference: str = "#" + self.join(self.title, "-").lower()
-# Next, create white space based on the level of the item and the minimum indent level.
-    whitespace: str = "\t" * (self.level - self.min_indent)
-# Finally, return the full list item.
-    return whitespace + "* [" + title_text + "](" + title_reference + ")"
 
-  
+
+class MDTOCItem(object):
+    # This is the constructor function, which sets up the object.
+    def __init__(self, level: int, title: Strings, min_indent: int = 1):
+        # Set some important properties.
+        self.title: Strings = title
+        self.level: int = level
+        self.min_indent: int = max(0, min_indent)
+
+# This function joins a given list of strings with a given separator.
+    def join(self, words: Strings, separator: str = " ") -> str:
+        # Initialise the text variable to an empty string.
+        text: str = ""
+        # Loop through each of the words.
+        for word in words:
+            # Append the word, as well as a proceeding separator, to the text variable.
+            text += separator + word
+        # Remove the first proceeding separator from text and then return it.
+        separator_end: int = len(separator)
+        text = text[separator_end:]
+        return text
+
+# This function returns a non-linked list item representing this list item.
+    def get_list_item_nonlinked(self) -> str:
+        # First, get a textual representation of the title.
+        title_text: str = self.join(self.title, " ")
+# Next, create white space based on the level of the item and the minimum indent level.
+        whitespace: str = "\t" * (self.level - self.min_indent)
+# Finally, return the full list item.
+        return whitespace + "* " + title_text
+
+    # This function returns a linked list item representing this list item.
+    def get_list_item_linked(self) -> str:
+        # First, get a textual representation of the title.
+        title_text: str = self.join(self.title, " ")
+# Also, get a textual representation of a link reference to this heading.
+        title_reference: str = "#" + self.join(self.title, "-").lower()
+# Next, create white space based on the level of the item and the minimum indent level.
+        whitespace: str = "\t" * (self.level - self.min_indent)
+# Finally, return the full list item.
+        return whitespace + "* [" + title_text + "](" + title_reference + ")"
+
 # Create the MDTOC class, which handles everything to do with the table of contents.
+
+
 class MDTOC(object):
-  # Define the constructor function.
-  def __init__(self, file: File, linked: bool=False, min_indent: int=1):
-# Set some important properties.
-    self.linked: bool = linked
-    self.min_indent: int = max(0, min_indent)
-    # Set the lines' property based on the return value of the get_file_contents function.
-    self.lines: Strings = self.get_file_contents(file)
-    # Create the regexp object used to match lines.
-    self.heading_re = re.compile("^\s*#+\\s.*$")
-    # This list will hold each matched line.
+    # Define the constructor function.
+    def __init__(self, file: File, linked: bool = False, min_indent: int = 1):
+        # Set some important properties.
+        self.linked: bool = linked
+        self.min_indent: int = max(0, min_indent)
+        # Set the lines' property based on the return value of the get_file_contents function.
+        self.lines: Strings = self.get_file_contents(file)
+        # Create the regexp object used to match lines.
+        self.heading_re = re.compile("^\\s*#+\\s.*$")
 # Call the 'get_headings' function to get all headings from the document.
-    self.headings: Strings = self.get_headings()
-    # Next, get a list of MDTOCItems using the 'get_items' function.
-    self.items = self.get_items()
-  
-# This function uses re to find all headings within the markdown document.
-  def get_headings(self) -> Strings:
-# Initialise the list of headings.
-    headings: Strings = []
-    # Loop through all the lines of the file.
-    for line in self.lines:
-      # Trim out any unneeded whitespace.
-      line = line.strip()
-      # Match the line to the heading re.
-      match = self.heading_re.match(line)
-      # Check if this line matches the heading pattern.
-      if bool(match) == True:
-        # We have a match, so add it to our list of headings.
-        headings.append(line)
+        # This list will hold each matched line.
+        self.headings: Strings = self.get_headings()
+        # Next, get a list of MDTOCItems using the 'get_items' function.
+        self.items = self.get_items()
+
+# This function uses the pre-defined re to find all headings within the markdown document.
+    def get_headings(self) -> Strings:
+        # Initialise the list of headings.
+        headings: Strings = []
+        # Loop through all the lines of the file.
+        for line in self.lines:
+            # Trim out any unneeded whitespace.
+            line = line.strip()
+            # Match the line to the heading re.
+            match = self.heading_re.match(line)
+            # Check if this line matches the heading pattern.
+            if bool(match) == True:
+                # We have a match, so add it to our list of headings.
+                headings.append(line)
 # Return the list of headings.
-    return headings
-  
-  # This function generates and returns a list of MDTOCItem objects to be used to generate the table of contents.
-  def get_items(self) -> List[MDTOCItem]:
-# Initialise the items list.
-    items: List[MDTOCItem] = []
-    # Now that we have all the headings in the document, loop through them.
-    for heading in self.headings:
-      # Find out what level of heading this is by getting the amount of '#' characters at the start.
-      whitespace_re = re.compile("\s")
-      words: Strings = whitespace_re.split(heading)
-      level: int = len(words[0])
-      # Next, create an MDTOCItem object representing this item.
-      li: MDTOCItem = MDTOCItem(level, words[1:], self.min_indent)
-      items.append(li)
-    # Finally, return the list of list items.
-    return items
-  
+        return headings
+
+    # This function generates and returns a list of MDTOCItem objects to be used to generate the table of contents.
+    def get_items(self) -> List[MDTOCItem]:
+        # Initialise the items list.
+        items: List[MDTOCItem] = []
+        # Now that we have all the headings in the document, loop through them.
+        for heading in self.headings:
+            # Find out what level of heading this is by getting the amount of '#' characters at the start.
+            whitespace_re = re.compile("\s")
+            words: Strings = whitespace_re.split(heading)
+            level: int = len(words[0])
+            # Next, create an MDTOCItem object representing this item.
+            li: MDTOCItem = MDTOCItem(level, words[1:], self.min_indent)
+            items.append(li)
+        # Finally, return the list of list items.
+        return items
+
 # This function takes either a path or a file object, returning always the file's contents.
-  def get_file_contents(self, file: File) -> Strings:
-    # Check if the passed 'file' argument is a string.
-    if type(file) == str:
-      # It is, so open a file with this path.
-      file: IO['TextIO'] = open(file, "r")
-    # We've got a file object regardless now, so return it's contents as a list of lines.
-    text: str = file.read()
-    lines: Strings = text.split("\n")
-    return lines
-  
-  # This function returns the markdown representation of the table of contents.
-  def get_toc(self) -> Strings:
-    # Initiate the text variable to hold the returned TOC.
-    text: str = ""
-    # Loop through all of the list items in the items list.
-    for item in self.items:
-      # Check whether we need to return linked or non-linked list items as part of the TOC.
-      if self.linked == False:
-      # Append the returned text of the current item's to the lines list.
-        text += item.get_list_item_nonlinked() + linesep
-      # If we do want links:
-      else:
-      # Append the returned text of the current item's to the lines list.
-        text += item.get_list_item_linked() + linesep
-    # Finally, return the text.
-    return text
+    def get_file_contents(self, file: File) -> Strings:
+        # Check if the passed 'file' argument is a string.
+        if type(file) == str:
+            # It is, so open a file with this path.
+            file: IO['TextIO'] = open(file, "r")
+        # We've got a file object regardless now, so return it's contents as a list of lines.
+        text: str = file.read()
+        lines: Strings = text.split("\n")
+        return lines
+
+    # This function returns the markdown representation of the table of contents.
+    def get_toc(self) -> Strings:
+        # Initiate the text variable to hold the returned TOC.
+        text: str = ""
+        # Loop through all of the list items in the items list.
+        for item in self.items:
+            # Check whether we need to return linked or non-linked list items as part of the TOC.
+            if self.linked:
+                # If we do want links:
+                # Append the returned text of the current item's to the lines list.
+                text += item.get_list_item_linked() + linesep
+                # If we don't want links
+            else:
+                # Append the returned text of the current item's to the lines list.
+                text += item.get_list_item_nonlinked() + linesep
+        # Finally, return the text.
+        return text
